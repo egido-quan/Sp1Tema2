af6a60b06b162556600a7051691527378729c951
egido-quan <isidro.zapater@gmail.com>
2024-06-30 09:50:16 +0200

    Informo de qué número eliminamos los múltiplos en cada criba

--- NIVEL_3/Ejercicio_1.php
+++ NIVEL_3/Ejercicio_1.php
@@ -10,7 +10,7 @@
     $numeros = [];
 
     //Número límite
-    $num = 44; 
+    $num = 45; 
     echo"CRIBA DE ERASTÓTENES PARA EL NÚMERO " . $num . "<br/>";
 
     //Creo un array de todos los números entre el 2 y el límite
@@ -28,20 +28,20 @@
     //Doble loop:
     //Empezando desde el primer número del array, voy borrando todos elementos del array que son
     //múltiplos suyos. Cuando he acabado con los múltiplos del primero del array, hago lo mismo
-    //con el siguiente y así sucesiavamente hasta llegar a la raíz cuadrada del número límite. 
+    //con el siguiente y así sucesiavamente hasta llegar a la raíz cuadrada del número límite, donde paramos.
     for ($i = 0; $numeros[$i]*$numeros[$i] <= $num; $i++) {
         for ($j = $i + 1; $j <= count($numeros); $j++) {
             if (($numeros[$j] % $numeros[$i]) == 0) {
                 array_splice($numeros, $j,1);
             }
         }
-        echo  "<br/>Criba número " . ($i + 1) . "<br/>";
+        echo  "<br/>Criba número " . ($i + 1) . ". Eliminamos los mútiplos de " . $numeros[$i] . "<br/>";
         for ($j = 0; $j <= count($numeros); $j++) {
             echo $numeros[$j] . " ";
         }
         echo "<br/>";
     }
-    echo "<br/> Listado final de número primos menores o iguales al " . $num . "<br/>";
+    echo "<br/> Listado final de números primos menores o iguales al " . $num . "<br/>";
     for ($j = 0; $j <= count($numeros); $j++) {
         echo $numeros[$j] . " ";
     }
